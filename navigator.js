6b15a8a0c03aa76931857f97de81fa9534ac2e49
Michael Zandstra <michael@tunnel.ninja>
2021-05-17 10:58:01 -0700

    change import to require

--- navigator.js
+++ navigator.js
@@ -1,4 +1,4 @@
-export const navigate = (screenName, params) => {
+exports.navigate = (screenName, params) => {
   // assume that is function correctly navigates to
   // to the named screen with the given parameters
 }
